dbcaae0140a8b79026f03ac060fbeafe578df36f
Artem <139005232+CyborgNinjaHat@users.noreply.github.com>
2026-04-14 11:39:08 +0000

    feat: implement custom sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,20 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const compare = compareFunction
+      ? (a, b) => compareFunction(a, b) > 0
+      : (a, b) => String(a) > String(b);
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (compare(this[j], this[j + 1])) {
+          [this[j], this[j + 1]] = [this[j + 1], this[j]];
+        }
+      }
+    }
+
+    return this;
   };
 }
 
