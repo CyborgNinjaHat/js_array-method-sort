94e3fda5914dcba24b1287c43560fef31a5ca4f3
Artem <139005232+CyborgNinjaHat@users.noreply.github.com>
2026-04-15 20:12:39 +0000

    refactor: improve default comparison logic in custom sort method

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,8 +1,20 @@
 'use strict';
 
-/**
- * Implement method Sort
- */
+function defaultCompare(a, b) {
+  const first = String(a);
+  const second = String(b);
+
+  if (first < second) {
+    return -1;
+  }
+
+  if (first > second) {
+    return 1;
+  }
+
+  return 0;
+}
+
 function applyCustomSort() {
   [].__proto__.sort2 = function (compareFunction) {
     const isInvalidCompareFunction =
@@ -12,13 +24,11 @@ function applyCustomSort() {
       throw new TypeError('compareFunction must be a function');
     }
 
-    const compare = compareFunction
-      ? (a, b) => compareFunction(a, b) > 0
-      : (a, b) => String(a) > String(b);
+    const compare = compareFunction ?? defaultCompare;
 
     for (let i = 0; i < this.length; i++) {
       for (let j = 0; j < this.length - i - 1; j++) {
-        if (compare(this[j], this[j + 1])) {
+        if (compare(this[j], this[j + 1]) > 0) {
           [this[j], this[j + 1]] = [this[j + 1], this[j]];
         }
       }
